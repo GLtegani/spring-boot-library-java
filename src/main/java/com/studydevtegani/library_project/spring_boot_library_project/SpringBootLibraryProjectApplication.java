4a25bc25c20f83cdbca49914217f894d9d8d4bc3
Gabriel <gabrielbtegani@gmail.com>
2024-08-30 13:02:22 -0300

    Initial Setup

---
+++ src/main/java/com/studydevtegani/library_project/spring_boot_library_project/SpringBootLibraryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.studydevtegani.library_project.spring_boot_library_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLibraryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLibraryProjectApplication.class, args);
+	}
+
+}
